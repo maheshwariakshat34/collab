9a88e2695e9f83e6e32b5f20fcd93003f64522c8
Ashish Kumar Yadav <ashishkumar12333ak@gmail.com>
2025-08-06 21:52:47 +0530

    Multiplication of two numbers

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+
+
+#Multiplying Two Static Numbers
+
+public class MultiplyStatic {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 10;
+        int result = num1 * num2;
+        System.out.println("The product is: " + result);
+    }
+}
+
